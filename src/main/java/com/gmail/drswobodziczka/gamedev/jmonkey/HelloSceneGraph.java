84e7ece473db2f3deed6da2993d5b4f07100ca64
Wawrzkowicz, Rafal <ext-rafal.wawrzkowicz@f-secure.com>
2020-04-25 22:59:55 +0200

    first hello scene graph -- create pseudoworld of 1000 spheres and boxes with random position and color

--- src/main/java/com/gmail/drswobodziczka/gamedev/jmonkey/HelloSceneGraph.java
+++ src/main/java/com/gmail/drswobodziczka/gamedev/jmonkey/HelloSceneGraph.java
@@ -7,7 +7,13 @@ import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Sphere;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import static com.jme3.math.ColorRGBA.randomColor;
 
 /**
  * Sample 2 - How to use nodes as handles to manipulate objects in the scene.
@@ -23,32 +29,51 @@ public class HelloSceneGraph extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-
-        /* create a blue box at coordinates (1,-1,1) */
-        Geometry blue = createBox(new Vector3f(1, -1, 1), ColorRGBA.Blue);
-        /* create a red box straight above the blue one at (1,3,1) */
-        Geometry red = createBox(new Vector3f(1, 3, 1), ColorRGBA.Red);
-
-        /* Create a pivot node at (0,0,0) and attach it to the root node */
+        // Create a pivot node at (0,0,0) and attach it to the root node
         Node pivot = new Node("pivot");
-
-
-        /* Attach the two boxes to the *pivot* node. (And transitively to the root node.) */
-        pivot.attachChild(blue);
-        pivot.attachChild(red);
-        /* Rotate the pivot node: Note that both boxes have rotated! */
-        pivot.rotate(.4f, .4f, 0f);
         rootNode.attachChild(pivot); // put this node in the scene
+
+        // create geometries amd attach to the *pivot* node. (And transitively to the root node.)
+        Random random = new Random();
+        IntStream.range(0, 1000)
+                .forEach((num) -> {
+                    if (random.nextBoolean()) {
+                        pivot.attachChild(createBox(randomVector(), randomColor()));
+                    } else {
+                        pivot.attachChild(createSphere(randomVector(), randomColor()));
+                    }
+                });
+
+        /* Rotate the pivot node: Note that all geometries have rotated! */
+        pivot.rotate(.4f, .4f, 0f);
+    }
+
+    @NotNull
+    private Vector3f randomVector() {
+        Random random = new Random();
+        int x = random.nextInt(100) * (random.nextBoolean() ? 1 : -1);
+        int y = random.nextInt(100) * (random.nextBoolean() ? 1 : -1);
+        int z = random.nextInt(100) * (random.nextBoolean() ? 1 : -1);
+        return new Vector3f(x, y, z);
     }
 
     @NotNull
     private Geometry createBox(Vector3f translation, ColorRGBA color) {
         Box box = new Box(1, 1, 1);
-        Material material = new Material(assetManager,
-                "Common/MatDefs/Misc/Unshaded.j3md");
+        Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         material.setColor("Color", color);
-
         Geometry geometry = new Geometry("Box", box);
+        geometry.setLocalTranslation(translation);
+        geometry.setMaterial(material);
+        return geometry;
+    }
+
+    @NotNull
+    private Geometry createSphere(Vector3f translation, ColorRGBA color) {
+        Sphere sphere = new Sphere(30, 30, 1);
+        Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        material.setColor("Color", color);
+        Geometry geometry = new Geometry("Sphere", sphere);
         geometry.setLocalTranslation(translation);
         geometry.setMaterial(material);
         return geometry;
