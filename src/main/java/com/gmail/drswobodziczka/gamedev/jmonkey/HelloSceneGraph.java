d68a55da7898a8fc0033e6181646d06dfa852584
Wawrzkowicz, Rafal <ext-rafal.wawrzkowicz@f-secure.com>
2020-04-26 01:32:21 +0200

    first hello scene graph -- tried Spatial rotation, scaling and translocation

--- src/main/java/com/gmail/drswobodziczka/gamedev/jmonkey/HelloSceneGraph.java
+++ src/main/java/com/gmail/drswobodziczka/gamedev/jmonkey/HelloSceneGraph.java
@@ -3,6 +3,7 @@ package com.gmail.drswobodziczka.gamedev.jmonkey;
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
@@ -40,6 +41,10 @@ public class HelloSceneGraph extends SimpleApplication {
                 "Common/MatDefs/Misc/ShowNormals.j3md");
         thing.setMaterial(mat);
         rootNode.attachChild(thing);
+
+        thing.rotate(45* FastMath.DEG_TO_RAD, 45* FastMath.DEG_TO_RAD, 45* FastMath.DEG_TO_RAD);
+        thing.setLocalTranslation(new Vector3f(1, 1, -10));
+        thing.scale( 10.0f, 0.1f, 1.0f );
     }
 
     @NotNull
