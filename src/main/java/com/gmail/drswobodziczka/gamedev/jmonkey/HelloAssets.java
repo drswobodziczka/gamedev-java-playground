128e4c1abaa9a502d6f8febc8cdbc3e83b33c689
Wawrzkowicz, Rafal <ext-rafal.wawrzkowicz@f-secure.com>
2020-04-26 19:26:20 +0200

    hello assets: load scene from ZIP file using ZipLocator; extracted methods

--- src/main/java/com/gmail/drswobodziczka/gamedev/jmonkey/HelloAssets.java
+++ src/main/java/com/gmail/drswobodziczka/gamedev/jmonkey/HelloAssets.java
@@ -1,6 +1,7 @@
 package com.gmail.drswobodziczka.gamedev.jmonkey;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.asset.plugins.ZipLocator;
 import com.jme3.font.BitmapText;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
@@ -9,9 +10,12 @@ import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
+import org.jetbrains.annotations.NotNull;
 
-/** Sample 3 - how to load an OBJ model, and OgreXML model,
- * a material/texture, or text. */
+/**
+ * Sample 3 - how to load an OBJ model, and OgreXML model,
+ * a material/texture, or text.
+ */
 public class HelloAssets extends SimpleApplication {
 
     public static void main(String[] args) {
@@ -22,51 +26,86 @@ public class HelloAssets extends SimpleApplication {
     @Override
     public void simpleInitApp() {
 
-        // add teapot
-        Spatial teapot = assetManager.loadModel("Models/Teapot/Teapot.obj");
-        Material mat_default = new Material(
-                assetManager, "Common/MatDefs/Misc/ShowNormals.j3md");
-        teapot.setMaterial(mat_default);
+        // attach teapot
+        Spatial teapot = createTeapotSpacialFromModel();
         rootNode.attachChild(teapot);
 
         // Create a wall with a simple texture from test_data
-        Box box = new Box(2.5f,2.5f,1.0f);
-        Spatial wall = new Geometry("Box", box );
-        Material mat_brick = new Material(
-                assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat_brick.setTexture("ColorMap",
-                assetManager.loadTexture("Textures/Terrain/BrickWall/BrickWall.jpg"));
-        wall.setMaterial(mat_brick);
-        wall.setLocalTranslation(2.0f,-2.5f,0.0f);
+        Spatial wall = createWallWIthTexture();
         rootNode.attachChild(wall);
 
         // Display a line of text with a default font
-        guiNode.detachAllChildren();
-        guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
-        BitmapText helloText = new BitmapText(guiFont, false);
-        helloText.setSize(guiFont.getCharSet().getRenderedSize());
-        helloText.setText("I'm dead;y ninja teapot!");
-        helloText.setLocalTranslation(300, helloText.getLineHeight(), 0);
-        guiNode.attachChild(helloText);
+        displayHelloMessageOnGuiNode();
 
         // Load a model from test_data (OgreXML + material + texture)
-        Spatial ninja = assetManager.loadModel("Models/Ninja/Ninja.mesh.xml");
-        ninja.scale(0.05f, 0.05f, 0.05f);
-        ninja.rotate(0.0f, -3.0f, 0.0f);
-        ninja.setLocalTranslation(0.0f, -5.0f, -2.0f);
+        Spatial ninja = createNinjaFromMeshModel();
         rootNode.attachChild(ninja);
         // You must add a light to make the model visible
         DirectionalLight sun = new DirectionalLight();
         sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f));
         rootNode.addLight(sun);
 
+        Spatial elephant = createElephantFromMeshModel();
+        rootNode.attachChild(elephant);
+
+//        Spatial scene = assetManager.loadModel("Scenes/ManyLights/Main.scene");
+//        rootNode.attachChild(scene);
+
+        // load scene using assert menager and zip locator
+        assetManager.registerLocator("town.zip", ZipLocator.class);
+        Spatial gameLevel = assetManager.loadModel("main.scene");
+        gameLevel.setLocalTranslation(0, -5.2f, 0);
+        gameLevel.setLocalScale(2);
+        rootNode.attachChild(gameLevel);
+    }
+
+    @NotNull
+    private Spatial createElephantFromMeshModel() {
         Spatial elephant = assetManager.loadModel("Models/Elephant/Elephant.mesh.xml");
         elephant.scale(.15f);
         elephant.move(new Vector3f(-2, 0, -15));
-        elephant.rotate(0, 180* FastMath.DEG_TO_RAD, 0);
-        rootNode.attachChild(elephant);
+        elephant.rotate(0, 180 * FastMath.DEG_TO_RAD, 0);
+        return elephant;
+    }
 
-        Spatial scene = assetManager.loadModel("Scenes/ManyLights/Main.scene");
-        rootNode.attachChild(scene);
+    @NotNull
+    private Spatial createNinjaFromMeshModel() {
+        Spatial ninja = assetManager.loadModel("Models/Ninja/Ninja.mesh.xml");
+        ninja.scale(0.05f, 0.05f, 0.05f);
+        ninja.rotate(0.0f, -3.0f, 0.0f);
+        ninja.setLocalTranslation(0.0f, -5.0f, -2.0f);
+        return ninja;
+    }
+
+    private void displayHelloMessageOnGuiNode() {
+        guiNode.detachAllChildren();
+        guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
+        BitmapText helloText = new BitmapText(guiFont, false);
+        helloText.setSize(guiFont.getCharSet().getRenderedSize());
+        helloText.setText("I'm dead;y ninja createTeapotSpacialFromModel!");
+        helloText.setLocalTranslation(300, helloText.getLineHeight(), 0);
+        guiNode.attachChild(helloText);
+    }
+
+    @NotNull
+    private Spatial createWallWIthTexture() {
+        Box box = new Box(2.5f, 2.5f, 1.0f);
+        Spatial wall = new Geometry("Box", box);
+        Material mat_brick = new Material(
+                assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat_brick.setTexture("ColorMap",
+                assetManager.loadTexture("Textures/Terrain/BrickWall/BrickWall.jpg"));
+        wall.setMaterial(mat_brick);
+        wall.setLocalTranslation(2.0f, -2.5f, 0.0f);
+        return wall;
+    }
+
+    @NotNull
+    private Spatial createTeapotSpacialFromModel() {
+        Spatial teapot = assetManager.loadModel("Models/Teapot/Teapot.obj");
+        Material mat_default = new Material(
+                assetManager, "Common/MatDefs/Misc/ShowNormals.j3md");
+        teapot.setMaterial(mat_default);
+        return teapot;
     }
 }
